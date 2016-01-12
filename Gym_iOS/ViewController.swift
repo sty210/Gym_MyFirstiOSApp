1fca4772beb382b74b87d3617f484dd241cd30cc
TaeYang Song <tysong@woowahan.com>
2016-01-12 17:31:00 +0900

    Initial Commit

---
+++ Gym_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gym_iOS
+//
+//  Created by elite on 2016. 1. 12..
+//  Copyright © 2016년 sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
